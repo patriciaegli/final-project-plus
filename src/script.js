30466a360818cb4c301c28b78ea179f0cd0678e8
patriciaegli <157884380+patriciaegli@users.noreply.github.com>
2024-02-10 12:31:29 +0100

    update

--- src/script.js
+++ src/script.js
@@ -53,7 +53,36 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast">
+            <div class="row">
+              <div class="col-2">
+                <div class="weather-forecast-day">${day} </div>
+                 <div class="weather-forecast-icon">🌤️</div>
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12°</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Zurich");
+searchCity("Zürich");
+displayForecast();
